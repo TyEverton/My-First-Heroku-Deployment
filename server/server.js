6e9e9d8efac3779c7e772b13f850d1e7c26e194f
Ty Everton <everton_915@yahoo.com>
2021-06-24 13:39:11 -0600

    first

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+
+const app = express()
+
+app.get('/', function(req, res) {
+  
+  res.sendFile( path.join(__dirname, '../index.html') )
+
+})
+
+const port = process.env.PORT || 4915
+
+app.listen(port, function () {
+  console.log(`Server rockout out at ${port}`)
+})
+
